6dd3596a93265b93af9dd610c0c16bc1adeb6c7a
Akira Kawamata <autumn@piedey.co.jp>
2020-09-07 09:42:34 +0900

    fix "2"

--- StringEnd/StringEnd/Program.cs
+++ StringEnd/StringEnd/Program.cs
@@ -16,11 +16,11 @@ class Program
         Console.WriteLine($"文字列の最後の1文字は【{ch2}】です。");
 
         // 最後の2文字が指定の文字か判定する
-        if (src.EndsWith("瞳子")) Console.WriteLine("文字列の最後の1文字は【瞳子】です。");
+        if (src.EndsWith("瞳子")) Console.WriteLine("文字列の最後の2文字は【瞳子】です。");
         // LINQを使わない方法で最後の2文字を取得する
         string s1 = src.Substring(src.Length - 2);
         Console.WriteLine($"文字列の最後の2文字は【{s1}】です。");
-        // LINQを使う方法で最後の1文字を取得する
+        // LINQを使う方法で最後の2文字を取得する
         string s2 = new string(src.TakeLast(2).ToArray());
         Console.WriteLine($"文字列の最後の2文字は【{s2}】です。");
     }
