9a5aba078c2fc3e6f83683ced8d0ab6a01d4e23c
Akira Kawamata <autumn@piedey.co.jp>
2020-09-08 09:13:26 +0900

    add SameNameMethod

---
+++ SameNameMethod/SameNameMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    class A
+    {
+        // 通常メソッド
+        internal void MyMethodA()
+        {
+            Console.WriteLine("I'm MyMethodA in class A.");
+        }
+        // 仮想メソッド
+        internal virtual void MyMethodB()
+        {
+            Console.WriteLine("I'm MyMethodB in class A.");
+        }
+    }
+
+    class B : A
+    {
+        // new付きメソッド
+        internal new void MyMethodA()
+        {
+            Console.WriteLine("I'm MyMethodA in class B.");
+        }
+        // override付きメソッド
+        internal override void MyMethodB()
+        {
+            Console.WriteLine("I'm MyMethodB in class B.");
+        }
+    }
+
+    static void Main()
+    {
+        // objは型B
+        B obj = new B();
+        // objTypeAは型A　(中味は同じ)
+        A objTypeA = obj;
+        obj.MyMethodA();
+        objTypeA.MyMethodA();
+        obj.MyMethodB();
+        objTypeA.MyMethodB();
+    }
+}
