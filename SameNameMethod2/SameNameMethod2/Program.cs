19a7b254047223d7cb120b8eef536692e47ea5a4
Akira Kawamata <autumn@piedey.co.jp>
2020-09-09 09:35:29 +0900

    add samenamemethod2

---
+++ SameNameMethod2/SameNameMethod2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface X1
+{
+    void Sub();
+}
+
+class A : X1
+{
+    public void Sub()
+    {
+        Console.WriteLine("I'm X1.Sub in A");
+    }
+}
+
+class B : A, X1
+{
+    void X1.Sub()
+    {
+        Console.WriteLine("I'm X1.Sub in B");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var b = new B();
+        b.Sub();
+        ((X1)b).Sub();
+    }
+}
