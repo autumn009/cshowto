d27af8ad61e205587e6406f2afb414537d85b57d
Akira Kawamata <autumn@piedey.co.jp>
2020-09-12 08:55:12 +0900

    add Negative

---
+++ Negative/Negative/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    private static void parseAndNegative(string s)
+    {
+        int.TryParse(s, out int v);
+        Console.Write(v);
+        Console.Write(">>>>");
+        v = -v;
+        Console.WriteLine(v);
+    }
+
+    private static void parseAndNegativeChecked(string s)
+    {
+        try
+        {
+            int.TryParse(s, out int v);
+            Console.Write(v);
+            Console.Write(">>>>");
+            checked
+            {
+                v = -v;
+            }
+            Console.WriteLine(v);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow");
+        }
+    }
+
+
+    static void Main()
+    {
+        parseAndNegative("-1234567890");
+        parseAndNegative("-2147483648");
+        parseAndNegativeChecked("-2147483648");
+    }
+}
