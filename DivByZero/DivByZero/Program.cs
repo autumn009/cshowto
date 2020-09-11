b2201404a76c8ce89c872bd7b13a326917cde583
Akira Kawamata <autumn@piedey.co.jp>
2020-09-11 09:45:30 +0900

    add DivByZero

---
+++ DivByZero/DivByZero/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    private static void div1(decimal x, decimal y)
+    {
+        Console.WriteLine(x / y);
+    }
+    private static void div2(double x, double y)
+    {
+        Console.WriteLine(x / y);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("div1の利用");
+        try
+        {
+            div1(0, 0);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("0で割りました。");
+        }
+        Console.WriteLine("div2の利用");
+        try
+        {
+            div2(0, 0);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("0で割りました。");
+        }
+    }
+}
