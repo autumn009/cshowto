1887ad847f5a7d9dea7fc2deff8fd33bd2c10029
Akira Kawamata <autumn@piedey.co.jp>
2020-09-13 08:57:30 +0900

    add forever

---
+++ for/for/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 無限ループしない
+        for (int i = 0; i <= 255; i++) Console.Write($"{i} ");
+        // 無限ループする
+        for (byte i = 0; i <= 255; i++) Console.Write($"{i} ");
+    }
+}
