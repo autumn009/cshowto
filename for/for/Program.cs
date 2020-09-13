e8d89db727c5813923392051d31a2bd37095eadf
Akira Kawamata <autumn@piedey.co.jp>
2020-09-13 09:10:47 +0900

    add bekkai

--- for/for/Program.cs
+++ for/for/Program.cs
@@ -6,6 +6,12 @@ class Program
     {
         // 無限ループしない
         for (int i = 0; i <= 255; i++) Console.Write($"{i} ");
+        // byte型で無限ループしない別解
+        for(byte i = 0; ;i++ )
+        {
+            Console.Write($"{i} ");
+            if ( i == 255) break;
+        }
         // 無限ループする
         for (byte i = 0; i <= 255; i++) Console.Write($"{i} ");
     }
