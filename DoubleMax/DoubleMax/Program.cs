281bd6a8ef277384cbb2c6c87808d590aa747d9b
Akira Kawamata <autumn@piedey.co.jp>
2020-09-14 09:21:25 +0900

    add  float/double max

---
+++ DoubleMax/DoubleMax/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float lastOne = 0.0f;
+        DateTimeOffset start = DateTimeOffset.Now;
+        for (float i = 1.0f; i < float.MaxValue;)
+        {
+            if (i > 16777200) Console.WriteLine(i);
+            if (lastOne == i)
+            {
+                Console.WriteLine($"カウントアップしていません! {i} {DateTimeOffset.Now - start}");
+                break;
+            }
+            lastOne = i;
+            i++;
+        }
+
+        DateTimeOffset startD = DateTimeOffset.Now;
+        for (double i = 1.0; i < double.MaxValue;i*=2)
+        {
+            double next = i + 1;
+            if (i == next)
+            {
+                Console.WriteLine($"カウントアップしていません! {i} {DateTimeOffset.Now - startD}");
+                return;
+            }
+        }
+    }
+}
