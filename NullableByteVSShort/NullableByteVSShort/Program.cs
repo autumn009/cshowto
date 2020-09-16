963e153e91a98eed40d57ad2d8bb96972539ed9d
Akira Kawamata <autumn@piedey.co.jp>
2020-09-16 09:17:16 +0900

    add NullableByteVSShort

---
+++ NullableByteVSShort/NullableByteVSShort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    private static T[] sub<T>(string label)
+    {
+        long start = Environment.WorkingSet;
+        T[] array = new T[100000];
+        Console.WriteLine($"{label} {Environment.WorkingSet - start}");
+        return array;
+    }
+
+
+    static void Main()
+    {
+        sub<byte?>("byte?");
+        sub<short>("short");
+    }
+}
