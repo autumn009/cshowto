ca01491caef9fb157191559d14f1ed87bfa0839d
Akira Kawamata <autumn@piedey.co.jp>
2020-09-17 09:07:34 +0900

    add SumOneLine

---
+++ SumOneLine/SumOneLine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // 作戦1　限定法
+        Console.WriteLine(Enumerable.Range(1,100).Where(c => (c % 2) == 0).Sum());
+        // 作戦1　加工法
+        Console.WriteLine(Enumerable.Range(1, 50).Sum(c=>c*2));
+    }
+}
