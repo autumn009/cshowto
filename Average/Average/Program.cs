d55c8156c301adcd5bce2afac8e0fcad23fc2f97
Akira Kawamata <autumn@piedey.co.jp>
2020-09-19 09:02:21 +0900

    add Avarage

---
+++ Average/Average/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // 整数の配列
+        int[] ar = { 1, 2, 3, 4 };
+        // 実はavgはdouble型
+        var avg = ar.Average();
+        Console.WriteLine($"Avarage is {avg}");
+    }
+}
