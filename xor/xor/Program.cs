c3edc437fc3142ea83ffa414c17dab2453feef22
Akira Kawamata <autumn@piedey.co.jp>
2020-09-21 09:25:57 +0900

    add xor

---
+++ xor/xor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    private static void sub1(bool isEnabled, bool flag)
+    {
+        if( isEnabled)
+        {
+            if (flag) flag = false; else flag = true;
+        }
+        Console.WriteLine(flag);
+    }
+
+    private static void sub2(bool isEnabled, bool flag)
+    {
+        Console.WriteLine(isEnabled ^ flag);
+    }
+
+    static void Main()
+    {
+        sub1(false, true);
+        sub1(true, true);
+        sub2(false, true);
+        sub2(true, true);
+    }
+}
