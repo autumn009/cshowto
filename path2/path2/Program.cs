9849e899ddff288ee9f7b46d4b607c0c2166354f
Akira Kawamata <autumn@piedey.co.jp>
2020-09-23 08:25:57 +0900

    add path2

---
+++ path2/path2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] ar = { "a", "b", "c" };
+        // 正攻法
+        var path1 = Path.Combine(ar[0], ar[1], ar[2]);
+        Console.WriteLine($"path1={path1}");
+        // これでもいける
+        var path2 = Path.Combine(ar);
+        Console.WriteLine($"path2={path2}");
+    }
+}
