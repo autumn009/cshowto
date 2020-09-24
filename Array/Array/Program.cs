fa7485dc1dfef55086fe778c35b29762182af5d4
Akira Kawamata <autumn@piedey.co.jp>
2020-09-24 09:04:24 +0900

    add Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 2次元配列
+        int[,] array1 = new int[3, 3];
+        array1[1, 2] = 123;
+        Console.WriteLine($"array1[1, 2] = {array1[1, 2]}");
+        // 配列の配列
+        int[][] array2 = new int[][] { new int[3], new int[3], new int[3] };
+        array2[1][2] = 123;
+        Console.WriteLine($"array2[1][2] = {array2[1][2]}");
+    }
+}
