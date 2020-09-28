5e7a280b712e273f3215781c6c78676a2a2f403e
Akira Kawamata <autumn@piedey.co.jp>
2020-09-28 09:38:24 +0900

    add ReplaceChar

---
+++ ReplaceChar/ReplaceChar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var s = "斉藤家の3男";
+        Console.WriteLine($"元文字列: {s}");
+        Console.WriteLine($"何人目の男の子?　{s[4]}");
+        s = s.Substring(0, 4) + "4" + s.Substring(5);
+        Console.WriteLine($"3→4修正後: {s}");
+    }
+}
