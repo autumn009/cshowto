054ef11c280b827144f9c7b399db720e24c48f24
Akira Kawamata <autumn@piedey.co.jp>
2020-09-29 09:46:29 +0900

    add ZWSP

---
+++ ZWSP/ZWSP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        char[] ar = { 'A', '\u200B', 'B' };
+        string s = new string(ar);
+        Console.WriteLine(s);
+        Debug.WriteLine(s);
+    }
+}
