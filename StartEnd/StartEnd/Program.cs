cd9afc5cc182b62c2756c391985e1891bd8327e8
Akira Kawamata <autumn@piedey.co.jp>
2020-09-30 10:54:51 +0900

    add StartEnd

---
+++ StartEnd/StartEnd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] ar = {
+            "クラウド","セフィロス","ティファ","エアリス","バレット"
+        };
+        foreach (var item in ar)
+        {
+            if (item.StartsWith("セ") && item.EndsWith("ス")) Console.WriteLine(item);
+        }
+    }
+}
