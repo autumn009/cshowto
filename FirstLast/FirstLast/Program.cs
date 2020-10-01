21847ba3d91ccb68e764a6bbb336bd050245524b
Akira Kawamata <autumn@piedey.co.jp>
2020-10-01 09:40:11 +0900

    add FirstLast

---
+++ FirstLast/FirstLast/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] ar = {
+            "クラウド","セフィロス","ティファ","エアリス","バレット"
+        };
+        foreach (var item in ar)
+        {
+            if (item.FirstOrDefault()== 'セ' && item.LastOrDefault()=='ス') Console.WriteLine(item);
+        }
+    }
+}
