9c3fecce1f958b942fe48a02c71b54fd0be47837
Akira Kawamata <autumn@piedey.co.jp>
2020-10-02 10:11:14 +0900

    add MyExeFileName

---
+++ MyExeFileName/MyExeFileName/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        var fileName = Assembly.GetExecutingAssembly().Location;
+        Console.WriteLine(fileName);
+    }
+}
