25e0f14dea27c57de33bae5bd7c80e2a50e2af5f
Akira Kawamata <autumn@piedey.co.jp>
2020-10-06 08:35:43 +0900

    add AtoZ

---
+++ AtoZ/AtoZ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // こうは書きたくない
+        var atoz1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Console.WriteLine(atoz1);
+        // まわりくどい
+        var sb = new StringBuilder();
+        for (int i = 0; i < 26; i++)
+        {
+            sb.Append((char)(i + 'A'));
+        }
+        var atoz2 = sb.ToString();
+        Console.WriteLine(atoz2);
+        // LINQ使用
+        var atoz3 = string.Concat(Enumerable.Range('A', 26).Select(c => (char)c));
+        Console.WriteLine(atoz3);
+        // NG
+        var ng = string.Concat(Enumerable.Range('A', 26));
+        Console.WriteLine(ng);
+    }
+}
