84df3982fcbba776e7017dbad6391d9140c8c7c2
Akira Kawamata <autumn@piedey.co.jp>
2020-10-10 11:32:37 +0900

    add PI

---
+++ PI/PI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // double型
+        double paiD = Math.PI;
+        Console.WriteLine(paiD);
+        // float型
+        float paiF = MathF.PI;
+        Console.WriteLine(paiF);
+        // double型
+        decimal paiE = 3.1415926535897932384626433833M; // 参考: 必要な桁数を考えて適切な桁数書こう
+        Console.WriteLine(paiE);
+    }
+}
