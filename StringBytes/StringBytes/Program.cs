26de5851aefe473eae307cbe49fa94b9e203a594
Akira Kawamata <autumn@piedey.co.jp>
2020-10-15 14:35:08 +0900

    add StringBytes

---
+++ StringBytes/StringBytes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var s = "Hella World";
+        Console.WriteLine(s);
+        var array = Encoding.UTF8.GetBytes(s);
+        foreach (var item in array) Console.Write($"{item} ");
+        Console.WriteLine();
+        array[4] = 111;
+        foreach (var item in array) Console.Write($"{item} ");
+        Console.WriteLine();
+        var d = Encoding.UTF8.GetString(array);
+        Console.WriteLine(d);
+    }
+}
+
+
