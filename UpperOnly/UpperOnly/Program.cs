31e524294c109a1cb31b2e935a9e844e0ebeca72
Akira Kawamata <autumn@piedey.co.jp>
2020-10-19 11:45:48 +0900

    add UpperOnly

---
+++ UpperOnly/UpperOnly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var s = "Jack Knife";
+        foreach (var item in s)
+        {
+            if (Char.IsUpper(item)) Console.Write(item);
+        }
+        Console.WriteLine();
+    }
+}
