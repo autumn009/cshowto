027de048b0bb89d616c6e3eef9555f16c7e5d6cf
Akira Kawamata <autumn@piedey.co.jp>
2020-10-23 09:35:29 +0900

    add ForeachCount

---
+++ ForeachCount/ForeachCount/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "熱海", "斑鳩", "宇野" };
+
+        foreach (var item in names.Select((name, number) => new { name, number }))
+        {
+            Console.WriteLine($"{item.number + 1} {item.name}");
+        }
+    }
+}
