d722e75b07ef2a5ace54f68746f1e0aef4ca10c3
Akira Kawamata <autumn@piedey.co.jp>
2020-10-24 09:10:52 +0900

    add SwapChar

---
+++ SwapChar/SwapChar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var s = "たろうくんの英語はA、国語はB";
+        var bad = s.Replace('A', 'B').Replace('B', 'A');
+        Console.WriteLine($"ダメなケース {bad}");
+        var good1 = s.Replace('A', '\0').Replace('B', 'A').Replace('\0', 'B');
+        Console.WriteLine($"良いケース1 {good1}");
+        var good2 = new StringBuilder();
+        foreach (var item in s) good2.Append(item switch { 'A' => 'B', 'B' => 'A', _ => item });
+        Console.WriteLine($"良いケース2 {good2.ToString()}");
+    }
+}
