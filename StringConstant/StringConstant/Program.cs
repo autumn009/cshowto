74176c71cd7e1f09aac6e93027dbcd3f51d789d5
Akira Kawamata <autumn@piedey.co.jp>
2020-10-25 16:14:47 +0900

    add StringConstant

---
+++ StringConstant/StringConstant/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var s1 = "\\{1 + 2}";
+        var s2 = @"\\{1 + 2}";
+        var s3 = $"\\{1 + 2}";
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+    }
+}
