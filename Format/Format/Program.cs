4b8faf0e042de405dad2ac8f6be9427847eb0305
Akira Kawamata <autumn@piedey.co.jp>
2020-10-26 08:17:42 +0900

    add Format

---
+++ Format/Format/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    private static void sub(string format)
+    {
+        Console.WriteLine(format,"Tom","School");
+    }
+
+
+    static void Main()
+    {
+        sub("{0} goes to {1}.");
+        sub("{1} へ {0} は行きました。");
+    }
+}
