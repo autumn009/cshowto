e455032af6356827646948baad053d028501a6c3
Akira Kawamata <autumn@piedey.co.jp>
2020-10-27 08:33:05 +0900

    add RealtimeKeyscan

---
+++ RealtimeKeyscan/RealtimeKeyscan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; ; i++)
+        {
+            Console.Write($"{i},");
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true);
+                Console.WriteLine();
+                Console.WriteLine($"何かのキーが押されました。押されたキーは{key.Key}です。プログラムを中断します。");
+                break;
+            }
+        }
+    }
+}
