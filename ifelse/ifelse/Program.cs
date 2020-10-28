81beb47858b01e751a9db4d7528f1dd577cda4d9
Akira Kawamata <autumn@piedey.co.jp>
2020-10-28 10:19:43 +0900

    add ifelse

---
+++ ifelse/ifelse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 結果が6 7 であって欲しいとき
+        for (int i = 0; i < 9; i++)
+        {
+            if (i > 5) if (i > 7) continue; else Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+        // 結果が0 1 2 3 4 5 であって欲しいとき
+        for (int i = 0; i < 9; i++)
+        {
+            if (i > 5) { if (i > 7) continue; } else Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+    }
+}
