63170415b14e4be5471ddf28782caa19743c886e
Akira Kawamata <autumn@piedey.co.jp>
2020-11-01 09:52:48 +0900

    add Capture

---
+++ Capture/Capture/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = 0;
+        sub1();
+        sub2();
+
+        void sub1()
+        {
+            a = 1234;
+        }
+
+        void sub2()
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
