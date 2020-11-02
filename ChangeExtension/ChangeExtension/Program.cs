2e713ad9b4582787abc232fdd53f50e6f8ab3ae8
Akira Kawamata <autumn@piedey.co.jp>
2020-11-02 09:59:05 +0900

    add ChangeExtension

---
+++ ChangeExtension/ChangeExtension/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] ar =
+        {
+            "test.doc",
+            "test.docx",
+            "test.txt",
+            "test.",
+            "test"
+        };
+        foreach (var item in ar)
+        {
+            var newname = Path.ChangeExtension(item, ".bak");
+            Console.WriteLine($"{item} => {newname}");
+        }
+    }
+}
