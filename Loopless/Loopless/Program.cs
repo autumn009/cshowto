3d0144263655bc4680765cea2749056d9154992f
Akira Kawamata <autumn@piedey.co.jp>
2020-11-03 09:17:22 +0900

    add Loopless

---
+++ Loopless/Loopless/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    private static void sub(int[] array)
+    {
+        string result = $"{string.Join('+', array)}={array.Sum()}";
+        Console.WriteLine(result);
+    }
+    static void Main()
+    {
+        int[] array1 = { 1, 2, 3 };
+        sub(array1);
+        int[] array2 = { 5, 8, 2, 11 };
+        sub(array2);
+    }
+}
