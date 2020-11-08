e53b284cf8e2a53c4c52d9ec77640d7bbd5e0c6f
Akira Kawamata <autumn@piedey.co.jp>
2020-11-09 08:44:44 +0900

    add TwoQuery

---
+++ TwoQuery/TwoQuery/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 2, 3, 5, 7, 11, 13, 17 };
+        // メソッド形式
+        var q1 = array.Where(c => c < 10).Select(c => c * c);
+        // クエリ式
+        var q2 = from c in array where c < 10 select c * c;
+        foreach (var item in q1) Console.Write(item);
+        Console.WriteLine();
+        foreach (var item in q2) Console.Write(item);
+        Console.WriteLine();
+    }
+}
