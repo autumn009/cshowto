2bc82f4c2bc6cc6fd262cf6602102fb748737f31
Akira Kawamata <autumn@piedey.co.jp>
2020-11-08 13:22:54 +0900

    add SameCond

---
+++ SameCond/SameCond/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(i);
+            if (!(i != 2 && i != 4)) Console.Write("[HIT]");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(i);
+            if (i == 2 || i == 4) Console.Write("[HIT]");
+        }
+        Console.WriteLine();
+    }
+}
