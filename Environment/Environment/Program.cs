d8bbe2853d9d9e8275e17e19761ea078a3744c5c
Akira Kawamata <autumn@piedey.co.jp>
2020-11-10 08:09:08 +0900

    add Environment

---
+++ Environment/Environment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool is64bitOS = Environment.Is64BitOperatingSystem;
+        bool is64bitProcess = Environment.Is64BitProcess;
+        bool is64bitPointer = IntPtr.Size == 8;
+        Console.WriteLine((is64bitOS? "64":"32")+"bit OSです。");
+        Console.WriteLine((is64bitProcess ? "64" : "32") + "bit プロセスです。");
+        Console.WriteLine((is64bitPointer ? "64" : "32") + "bit ポインターです。");
+    }
+}
