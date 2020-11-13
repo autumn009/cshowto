f5fd0b21531ebedeb9b8b75a6537f540e5b4e8cd
Akira Kawamata <autumn@piedey.co.jp>
2020-11-13 11:51:21 +0900

    add IntegerAndReal

---
+++ IntegerAndReal/IntegerAndReal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 100;
+        // 実数で1.2倍する
+        Console.WriteLine(a * 1.2);
+        // 整数だけで1.2倍する
+        Console.WriteLine(a * 12 / 10);
+    }
+}
