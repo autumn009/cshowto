bda6f7d1fd799642ebadb0b5f99fcca0c6566d37
Akira Kawamata <autumn@piedey.co.jp>
2020-11-17 09:02:40 +0900

    add StringReader

---
+++ StringReader/StringReader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    private static void fileReaderSub(TextReader reader)
+    {
+        for (; ; )
+        {
+            var s = reader.ReadLine();
+            if (s == null) break;
+            Console.WriteLine($"[{s}]");
+        }
+    }
+    static void Main()
+    {
+        string src = "Hello!\nMy Friends!\nWelcome!";
+        File.WriteAllText("sample.txt", src);
+        // ファイルから読み出す
+        using(var reader1 = File.OpenText("sample.txt"))
+        {
+            fileReaderSub(reader1);
+        }
+        // 文字列から読み出す
+        var reader2 = new StringReader(src);
+        fileReaderSub(reader2);
+    }
+}
