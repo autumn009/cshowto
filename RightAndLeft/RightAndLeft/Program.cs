8ef9729c47ed4dfc26d6ac086cee659e0f79830d
Akira Kawamata <autumn@piedey.co.jp>
2020-11-19 08:49:18 +0900

    add RightAndLeft

---
+++ RightAndLeft/RightAndLeft/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 12324;
+        Console.WriteLine("6文字幅で右に揃えろ");
+        Console.WriteLine($"[{x,6}]");
+        Console.WriteLine("6文字幅で左に揃えろ");
+        Console.WriteLine($"[{x,-6}]");
+    }
+}
+
