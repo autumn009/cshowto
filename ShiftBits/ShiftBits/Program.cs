fff63000bc1113e98e2ecfe765fd981c04b9348b
Akira Kawamata <autumn@piedey.co.jp>
2020-11-18 10:12:24 +0900

    add ShiftBits

---
+++ ShiftBits/ShiftBits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte high = 0x12;
+        byte low = 0x34;
+        ushort combined = (ushort)(low | high << 8);
+        Console.WriteLine($"{combined:x}");
+    }
+}
