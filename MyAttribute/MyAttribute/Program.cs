25e4f3b9f94bace518c88fdb877ca44448448048
Akira Kawamata <autumn@piedey.co.jp>
2020-11-23 08:39:38 +0900

    add MyAttribute

---
+++ MyAttribute/MyAttribute/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+class MyAttribute : Attribute
+{
+}
+
+class Program
+{
+    public int MyProperty1 { get; set; }
+    [My]
+    public int MyProperty2 { get; set; }
+
+
+    static void Main()
+    {
+        foreach (var item in typeof(Program).GetProperties())
+        {
+            Console.WriteLine($"{item.Name}は{nameof(MyAttribute)}属性を");
+            if (item.GetCustomAttribute(typeof(MyAttribute)) == null)
+                Console.WriteLine("持っていません。");
+            else
+                Console.WriteLine("持っています。");
+        }
+    }
+}
