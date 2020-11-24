6030fd18da504e38b4055cbb055cfbd8500dcb9e
Akira Kawamata <autumn@piedey.co.jp>
2020-11-24 09:09:59 +0900

    add DynamicType

---
+++ DynamicType/DynamicType/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class A
+{
+    public void Sub()
+    {
+        Console.WriteLine("I'm class A!");
+    }
+}
+
+class B
+{
+    public void Sub(string a = "Dummy")
+    {
+        Console.WriteLine("I'm class B!");
+    }
+}
+
+class Program
+{
+    private static void commonWork(dynamic arg)
+    {
+        arg.Sub();
+    }
+    static void Main()
+    {
+        commonWork(new A());
+        commonWork(new B());
+    }
+}
