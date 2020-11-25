309f9498b43377c95608b1cbf60852db5528d8b1
Akira Kawamata <autumn@piedey.co.jp>
2020-11-25 09:37:12 +0900

    add floatMaxOlusOne

---
+++ floatMaxPlusOne/floatMaxPlusOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        checked
+        {
+            try
+            {
+                int a = int.MaxValue;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(a);
+                    a++;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                float a = float.MaxValue;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(a);
+                    a++;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
