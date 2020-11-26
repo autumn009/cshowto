b53a4b450f7092c964070810383b58ac77bfd022
Akira Kawamata <autumn@piedey.co.jp>
2020-11-26 09:41:15 +0900

    add TimeEqual

---
+++ TimeEqual/TimeEqual/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (; ; )
+        {
+            var target = DateTimeOffset.Now.AddSeconds(1.0);
+            for (; ; )
+            {
+                if (DateTimeOffset.Now == target) break;
+                // ↓正しいコード
+                //if (DateTimeOffset.Now > target) break;
+            }
+            Console.WriteLine(DateTimeOffset.Now);
+        }
+    }
+}
