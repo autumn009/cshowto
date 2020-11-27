cf46de1e7d9baf95304d6539a3394a890416f3ed
Akira Kawamata <autumn@piedey.co.jp>
2020-11-27 10:38:30 +0900

    add beep

---
+++ beep/beep/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Working...");
+        Console.WriteLine("Done!");
+        Console.Beep();
+        Console.Beep(800,200);
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Beep(800+i*100, 200);
+        }
+    }
+}
