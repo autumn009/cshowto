ef675bbce71d9e851ba88fa6a29e07f0eb3016f9
Akira Kawamata <autumn@piedey.co.jp>
2020-11-30 10:10:04 +0900

    add Finallizer

---
+++ Fnalizer/Fnalizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Sample : IDisposable
+{
+    public Sample()
+    {
+        Console.WriteLine("私、産まれました!");
+    }
+    ~Sample()
+    {
+        Console.WriteLine("私、消えます!");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("私、Disposeされました!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // ファイナライザー?
+        var a = new Sample();
+        Console.WriteLine($"a of {a} is here!");
+        a = null;
+        GC.Collect();
+        // IDisposable インターフェース?
+        using var b = new Sample();
+        Console.WriteLine($"b of {b} is here!");
+    }
+}
