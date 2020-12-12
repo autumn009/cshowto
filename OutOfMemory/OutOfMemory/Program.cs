ee3f54b6e383063f877562bee87bc1c8db01c924
Akira Kawamata <autumn@piedey.co.jp>
2020-12-12 15:47:41 +0900

    add OutOfMemory

---
+++ OutOfMemory/OutOfMemory/Program.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var list = new List<byte[]>();
+            for (int i = 0; i < 100000; i++)
+            {
+                list.Add(new byte[100000000]);
+            }
+        }
+    }
