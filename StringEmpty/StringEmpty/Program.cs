aab05a25f3ed95a9d0b698a788b33ccf8237ee4b
Akira Kawamata <autumn@piedey.co.jp>
2020-12-12 15:40:52 +0900

    add StringEmpty

---
+++ StringEmpty/StringEmpty/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Enumerable.Empty<string>().Count());
+        Console.WriteLine(string.Empty.Count());
+    }
+}
