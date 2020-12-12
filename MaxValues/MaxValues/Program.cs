6b815591b4d58b4f11a47255131769053e3a497f
Akira Kawamata <autumn@piedey.co.jp>
2020-12-12 15:09:02 +0900

    add MaxValue

---
+++ MaxValues/MaxValues/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(sbyte.MaxValue);
+        Console.WriteLine(byte.MaxValue);
+        Console.WriteLine(short.MaxValue);
+        Console.WriteLine(ushort.MaxValue);
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(uint.MaxValue);
+        Console.WriteLine(long.MaxValue);
+        Console.WriteLine(ulong.MaxValue);
+        Console.WriteLine(float.MaxValue);
+        Console.WriteLine(double.MaxValue);
+        Console.WriteLine(decimal.MaxValue);
+    }
+}
